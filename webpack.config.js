dc8a79413c1da73ba3f874c7f0356f2cc00a42e1
Ahmad Mana <Kingahmed19991@gmail.com>
2022-06-29 01:41:42 +0300

    setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
+  watch: true,
   output: {
     path:path.resolve(__dirname, "dist"),
   },
