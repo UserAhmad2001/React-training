fc363bfc9ec31f6202960c3ee1f8144cdb4bd200
Ahmad Mana <Kingahmed19991@gmail.com>
2022-06-29 23:57:07 +0300

    react project card design

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 
 export default function Buttons(){
+    function clk(e){
+        e.preventDefault()
+    document.querySelector('.btn2')
+    .addEventListener('click',(e)=>{
+    var a = document.querySelector('.info').style.display
+    if(a==="flex"){
+        document.querySelector('.info').style.display = "none"
+    }else{
+        document.querySelector('.info').style.display = "flex"
+    }
+})
+    }
     return (
         <div className="buttons">
             <div className="btn1">Contact</div>
-            <div className="btn2">View Portfolio</div>
+            <div className="btn2" onClick={clk}>View Portfolio</div>
         </div>
     );
 }
+
